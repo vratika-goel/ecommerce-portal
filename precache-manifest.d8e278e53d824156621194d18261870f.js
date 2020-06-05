64fc9506807ddd90bac3e54f36b66228607c853d
vratika-goel <vratika.goel@hpe.com>
2020-06-05 13:25:15 +0530

    Updates

---
+++ precache-manifest.d8e278e53d824156621194d18261870f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b8f0ca475c1b4080000ac942c8fdf2",
+    "url": "/ecommerce-portal/index.html"
+  },
+  {
+    "revision": "9f82aa632a2ef4f5f512",
+    "url": "/ecommerce-portal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa064f75b8344e95139f",
+    "url": "/ecommerce-portal/static/js/2.86bc2dd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce-portal/static/js/2.86bc2dd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f82aa632a2ef4f5f512",
+    "url": "/ecommerce-portal/static/js/main.446177da.chunk.js"
+  },
+  {
+    "revision": "1789ea651eca324e94f5",
+    "url": "/ecommerce-portal/static/js/runtime-main.9efe7261.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecommerce-portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
