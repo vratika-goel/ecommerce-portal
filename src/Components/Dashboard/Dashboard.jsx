cfe0a5772a17684615c69765c87a1d7186ba0ac1
AkashMishraRF <akmishra@rodanandfields.com>
2020-06-06 12:05:42 +0530

    Added basic layout

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div className="container">
+            <div className="row header-wrapper">
+                Akash   
+            </div>
+            <div className="row body-wrapper">
+                Mishra
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
